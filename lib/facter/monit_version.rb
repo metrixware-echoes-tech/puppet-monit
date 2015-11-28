f38df845526e6d6f83fc811a7f3662f6c7b798f2
Phil Friderici <phil.friderici@i-tee.de>
2015-11-28 18:42:01 +0000

    Refactor monit_version and add spec test

--- lib/facter/monit_version.rb
+++ lib/facter/monit_version.rb
@@ -1,11 +1,14 @@
-if Facter::Util::Resolution.which('monit')
-  version = Facter::Util::Resolution.exec('monit -V 2>&1').match(/\d+\.\d+$/).to_s
+# A facter fact to determine the installed version of monit.
 
-  if version
-    Facter.add('monit_version') do
-      setcode do
-        version
-      end
+module Facter::Util::MonitVersion
+  class << self
+    def get_monit_version
+      monit_version = Facter::Util::Resolution.exec('monit -V 2>&1')
+      monit_version && monit_version.match(/\d+\.\d+$/).to_s
     end
   end
+end
+
+Facter.add(:monit_version) do
+  setcode { Facter::Util::MonitVersion.get_monit_version }
 end
