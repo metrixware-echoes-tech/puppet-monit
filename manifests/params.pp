2a2c57f3154b147f9d74e525aa0ccc9c9c53a66d
hdeadman <hal.deadman@gmail.com>
2016-10-29 00:29:37 -0400

    support ubuntu 16.04

--- manifests/params.pp
+++ manifests/params.pp
@@ -39,13 +39,13 @@ class monit::params {
           $default_file_content = 'startup=1'
           $service_hasstatus    = false
         }
-        'wheezy', 'jessie', 'precise', 'trusty': {
+        'wheezy', 'jessie', 'precise', 'trusty', 'xenial': {
           $default_file_content = 'START=yes'
           $service_hasstatus    = true
         }
         default: {
           fail("monit supports Debian 6 (squeeze), 7 (wheezy) and 8 (jessie) \
-and Ubuntu 10.04 (lucid), 12.04 (precise) and 14.04 (trusty). Detected lsbdistcodename is <${::lsbdistcodename}>.")
+and Ubuntu 10.04 (lucid), 12.04 (precise), 14.04 (trusty) and 16.04 (xenial). Detected lsbdistcodename is <${::lsbdistcodename}>.")
         }
 
       }
