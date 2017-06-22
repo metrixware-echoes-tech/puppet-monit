e2031d9fd496474ec957640182fe5d32290279a5
Tobias Knipping <knipping@tk-schulsoftware.de>
2017-06-22 19:01:38 +0200

    support for debian 9/stretch

--- manifests/params.pp
+++ manifests/params.pp
@@ -39,12 +39,12 @@ class monit::params {
           $default_file_content = 'startup=1'
           $service_hasstatus    = false
         }
-        'wheezy', 'jessie', 'precise', 'trusty', 'xenial': {
+        'wheezy', 'jessie', 'stretch', 'precise', 'trusty', 'xenial': {
           $default_file_content = 'START=yes'
           $service_hasstatus    = true
         }
         default: {
-          fail("monit supports Debian 6 (squeeze), 7 (wheezy) and 8 (jessie) \
+          fail("monit supports Debian 6 (squeeze), 7 (wheezy), 8 (jessie) and 9 (stretch) \
 and Ubuntu 10.04 (lucid), 12.04 (precise), 14.04 (trusty) and 16.04 (xenial). \
 Detected lsbdistcodename is <${::lsbdistcodename}>.")
         }
